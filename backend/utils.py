5863e8e45a0c5f68b9e4fe5b01ef26c0d3f635eb
Zhuo ZENG <zzengan@connect.ust.hk>
2024-12-03 18:08:48 -0500

    use singleton in connection

--- backend/utils.py
+++ backend/utils.py
@@ -1,6 +1,26 @@
+from mysql.connector import Error
 import mysql.connector
 from config import db_config
 
+class DatabaseConnection:
+    _instance = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            try:
+                cls._instance = super(DatabaseConnection, cls).__new__(cls)
+                cls._instance.connection = mysql.connector.connect(**db_config)
+            except Error as e:
+                print(f"Error: {e}")
+                cls._instance = None
+        return cls._instance
+
+    def get_connection(self):
+        if self._instance is None or not self._instance.connection.is_connected():
+            print("Re-establishing the connection...")
+            self._instance.connection = mysql.connector.connect(**db_config)
+        return self._instance.connection
+
 def get_db_connection():
-    connection = mysql.connector.connect(**db_config)
+    connection = DatabaseConnection().get_connection()
     return connection
