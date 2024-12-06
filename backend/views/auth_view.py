b45ef70f828ef5df289145b139d6dfef3810a91a
AL-SUN <sunjiaxin2002@163.com>
2024-12-06 00:57:28 -0500

    Change authentification to use flask-login

--- backend/views/auth_view.py
+++ backend/views/auth_view.py
@@ -1,28 +1,39 @@
-from flask import Blueprint, request, jsonify, session, redirect, url_for, flash
+from flask import Blueprint, request, jsonify, session, flash, current_app
 import mysql.connector
-import hashlib
-import os
-import base64
 from utils import get_db_connection  # Assuming your connection class is in db_connection.py
 from flask_cors import cross_origin
 
 from werkzeug.security import check_password_hash, generate_password_hash
+from flask_login import LoginManager, UserMixin
+from flask_login import login_user, logout_user, current_user, login_required
+
+# User class for flask-login: TBD
+class User(UserMixin):
+    def __init__(self, username, fname):
+        self.fname = fname
+        self.username = username
+        
+    def get_id(self):
+        return self.username
 
 
-auth_bp = Blueprint('auth_bp', __name__)
+auth_bp = Blueprint('auth_bp', __name__,url_prefix='/auth')
 
-def hash_password(password):
-    salt = os.urandom(16)  # Generate a random salt
-    salted_password = salt + password.encode('utf-8')
-    hashed_password = hashlib.sha256(salted_password).hexdigest()
-    return base64.b64encode(salt + hashed_password.encode('utf-8'))  # Store salt with the hash
+login_manager = LoginManager()
 
-def check_password(stored_encoded_password, provided_password):
-    stored_password = base64.b64decode(stored_encoded_password)
-    salt = stored_password[:16]  # Extract the salt
-    salted_provided_password = salt + provided_password.encode('utf-8')
-    hashed_provided_password = hashlib.sha256(salted_provided_password).hexdigest()
-    return stored_password[16:] == hashed_provided_password.encode('utf-8')
+@login_manager.user_loader
+def load_user(userName):
+    with get_db_connection().cursor() as cursor:
+        cursor.execute('SELECT * FROM Person WHERE userName = %s', (userName,))
+        columns = [column[0] for column in cursor.description]
+        res = cursor.fetchone()
+        res_dict = dict(zip(columns, res))
+        if len(res_dict) == 0:
+            return None
+        else:
+            username = res_dict.get("userName")
+            fname = res_dict.get("fname")
+            return User(username,fname)
 
 @auth_bp.route('/register', methods=['POST'])
 @cross_origin()
@@ -34,19 +45,18 @@ def register():
     lname = user_data['lname']
     email = user_data['email']
 
-    # hashed_password = hash_password(password)
     hashed_password = generate_password_hash(password)
+    print(f"Hashed Password {hashed_password}") # Debugging
 
     connection = get_db_connection()
     cursor = connection.cursor()
-
     try:
-        print(f"Hashed Password {hashed_password}")
         cursor.execute("INSERT INTO Person (userName, password, fname, lname, email) VALUES (%s, %s, %s, %s, %s)",
-                       (userName, hashed_password, fname, lname, email))
+                    (userName, hashed_password, fname, lname, email))
         connection.commit()
         return jsonify({"message": "User registered successfully"}), 201
     except mysql.connector.Error as err:
+        connection.rollback() # Rollback if error
         return jsonify({"error": str(err)}), 400
     finally:
         cursor.close()
@@ -59,25 +69,33 @@ def login():
     userName = user_data['userName']
     password = user_data['password']
 
-    connection = get_db_connection()
-    cursor = connection.cursor()
+    with get_db_connection().cursor() as cursor:
+        cursor.execute("SELECT * FROM person WHERE userName = %s", (userName,))
+        columns = [column[0] for column in cursor.description]
+        user = cursor.fetchone()
+        
+        if user and check_password_hash(user[1], password):
+            res_dict = dict(zip(columns, user))
+            login_user(User(res_dict.get("userName"),res_dict.get("fname"))) # Login user
+            return jsonify({"message": "Login success!"}), 200
+        else:
+            return jsonify({"error": "Invalid username or password"}), 401
 
-    cursor.execute("SELECT * FROM Person WHERE userName = %s", (userName,))
-    user = cursor.fetchone()
+@auth_bp.route('/logout')
+@cross_origin()
+def logout():
+    logout_user()
+    return jsonify({"message": "User logged out"}), 200
 
-    # if user and check_password(user[1].encode('utf-8'), password):
-    if user and check_password_hash(user[1], password):
-        session['userName'] = user[0]  # Store username in session
-        session['fname'] = user[2]
-        session['lname'] = user[3]
-        return jsonify({"message": "Login success!"}), 200
-    else:
-        flash("Login failed: Invalid username or password.")
-        return jsonify({"error": "Invalid username or password"}), 401
-
+# 不知道这个是干嘛的，就没改
 @auth_bp.route('/protected', methods=['GET'])
 @cross_origin()
 def protected_resource(): # test protected
     if 'userName' in session:
         return f"Welcome to the protected resource, {session['fname']} {session['lname']}!"
     return jsonify({"error": "Please login first"}), 401
+
+
+login_manager.login_view = 'auth_bp.login'
+def init_auth_app(app):
+    login_manager.init_app(app)
