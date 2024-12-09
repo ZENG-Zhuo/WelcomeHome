b877a70d2e8f2c4fd9659b774514197d7b06520c
AL-SUN <sunjiaxin2002@163.com>
2024-12-08 23:59:43 -0500

    file

--- backend/config.py
+++ backend/config.py
@@ -1,7 +1,7 @@
 db_config = {
     'user': 'root',
-    'password': 'TPGSecretPass',
+    'password': 'root',
     'host': 'localhost',
-    'port': "3307",
+    'port': "3306",
     'database': 'welcomehome',
 }
